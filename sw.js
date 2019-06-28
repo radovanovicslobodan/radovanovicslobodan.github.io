56626027d19a1b907051b3694dac1d1a3d2eb624
Boban <r.slobodan75@gmail.com>
2019-06-28 12:21:51 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3dbaa02a07873086350b.js"
+    "url": "webpack-runtime-386450a8a8cf0a385da0.js"
   },
   {
     "url": "app-6fe97679d140cd1aca51.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "23c9f2b74c2c1967b92ea6154f560934"
+    "revision": "89dbba8bd1df1d73b44caa50766c37ae"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
